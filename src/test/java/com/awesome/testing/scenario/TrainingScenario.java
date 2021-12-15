c681a5a2f186f5ee1110294e890ae235de000f8f
Slawek <s.radzyminski@ocado.com>
2021-12-15 08:43:33 +0100

    step4 - token

--- src/test/java/com/awesome/testing/scenario/TrainingScenario.java
+++ src/test/java/com/awesome/testing/scenario/TrainingScenario.java
@@ -3,16 +3,19 @@ package com.awesome.testing.scenario;
 import io.gatling.javaapi.core.ScenarioBuilder;
 
 import static com.awesome.testing.feeder.CredentialsFeeder.CREDENTIALS_FEEDER;
+import static com.awesome.testing.request.GetUsers.GET_ALL_USERS_REQUEST;
 import static com.awesome.testing.request.Login.LOGIN_REQUEST;
 import static com.awesome.testing.request.Register.REGISTER_REQUEST;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 
 public class TrainingScenario {
 
-    public static final ScenarioBuilder TRAINING_SCENARIO = scenario("Training scenarion")
+    public static final ScenarioBuilder TRAINING_SCENARIO = scenario("Training scenario")
             .feed(CREDENTIALS_FEEDER)
             .exec(REGISTER_REQUEST)
-            .pause(10)
-            .exec(LOGIN_REQUEST);
+            .pause(5)
+            .exec(LOGIN_REQUEST)
+            .pause(5)
+            .exec(GET_ALL_USERS_REQUEST);
 
 }
