5506054fffef933492e917a33a0b52ac246db83f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-04-27 16:51:25 +0200

    Building body via code

---
+++ src/test/java/com/awesome/testing/request/edit/dto/Roles.java
@@ -0,0 +1,7 @@
+package com.awesome.testing.request.edit.dto;
+
+public enum Roles {
+
+    ROLE_CLIENT, ROLE_ADMIN
+
+}
--- src/test/java/com/awesome/testing/scenario/TrainingScenario.java
+++ src/test/java/com/awesome/testing/scenario/TrainingScenario.java
@@ -6,6 +6,7 @@ import static com.awesome.testing.feeder.CredentialsFeeder.CREDENTIALS_FEEDER;
 import static com.awesome.testing.request.GetUsers.GET_ALL_USERS_REQUEST;
 import static com.awesome.testing.request.Login.LOGIN_REQUEST;
 import static com.awesome.testing.request.Register.REGISTER_REQUEST;
+import static com.awesome.testing.request.edit.EditUser.EDIT_USER_REQUEST;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 
 public class TrainingScenario {
@@ -16,6 +17,8 @@ public class TrainingScenario {
             .pause(5)
             .exec(LOGIN_REQUEST)
             .pause(5)
-            .exec(GET_ALL_USERS_REQUEST);
+            .exec(GET_ALL_USERS_REQUEST)
+            .pause(5)
+            .exec(EDIT_USER_REQUEST);
 
 }
