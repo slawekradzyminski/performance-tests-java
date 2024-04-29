3f940141f2177ef7a8bfb0f4069ebde4ae386966
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-04-29 12:14:43 +0200

    ye04

--- src/test/java/com/awesome/testing/scenario/TrainingScenario.java
+++ src/test/java/com/awesome/testing/scenario/TrainingScenario.java
@@ -3,8 +3,7 @@ package com.awesome.testing.scenario;
 import io.gatling.javaapi.core.ScenarioBuilder;
 
 import static com.awesome.testing.request.Login.LOGIN_REQUEST;
-import static io.gatling.javaapi.core.CoreDsl.csv;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.*;
 
 /**
  * Represents a single user journey
@@ -12,7 +11,7 @@ import static io.gatling.javaapi.core.CoreDsl.scenario;
 public class TrainingScenario {
 
     public static final ScenarioBuilder TRAINING_SCENARIO = scenario("Training scenario")
-            .feed(csv("data/credentials.csv").circular())
+            .feed(jsonFile("data/credentials.json").circular())
             .exec(LOGIN_REQUEST);
 
 }
