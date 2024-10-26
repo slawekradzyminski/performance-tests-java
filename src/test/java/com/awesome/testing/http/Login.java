8a03de2b12741c9515e5de472ed1ea7c5dbc576f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-10-26 11:15:12 +0200

    oct02

---
+++ src/test/java/com/awesome/testing/http/Login.java
@@ -0,0 +1,17 @@
+package com.awesome.testing.http;
+
+import io.gatling.javaapi.http.HttpRequestActionBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.ElFileBody;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class Login {
+
+    public static final HttpRequestActionBuilder LOGIN_REQUEST =
+            http("Login request")
+                    .post("/users/signin")
+                    .body(ElFileBody("bodies/adminLogin.json"))
+                    .check(status().is(200));
+
+}
--- src/test/java/com/awesome/testing/scenario/TrainingScenario.java
+++ src/test/java/com/awesome/testing/scenario/TrainingScenario.java
@@ -2,18 +2,13 @@ package com.awesome.testing.scenario;
 
 import io.gatling.javaapi.core.ScenarioBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.ElFileBody;
+import static com.awesome.testing.http.Login.LOGIN_REQUEST;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
-import static io.gatling.javaapi.http.HttpDsl.http;
-import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class TrainingScenario {
 
-    public static final ScenarioBuilder TRAINING_SCENARIO = scenario("Training scenario")
-            .exec(http("Admin login request")
-                    .post("/users/signin")
-                    .body(ElFileBody("bodies/adminLogin.json"))
-                    .check(status().is(200))
-            );
+    public static final ScenarioBuilder TRAINING_SCENARIO =
+            scenario("Training scenario")
+                    .exec(LOGIN_REQUEST);
 
 }
