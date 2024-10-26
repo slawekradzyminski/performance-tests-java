195f932035f375dbc14f0acc431775c96680c2ea
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-10-26 12:25:33 +0200

    oct05

--- src/test/java/com/awesome/testing/scenario/TrainingScenario.java
+++ src/test/java/com/awesome/testing/scenario/TrainingScenario.java
@@ -4,15 +4,14 @@ import io.gatling.javaapi.core.FeederBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 
 import static com.awesome.testing.http.Login.LOGIN_REQUEST;
-import static io.gatling.javaapi.core.CoreDsl.csv;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.*;
 
 public class TrainingScenario {
 
-    private static final FeederBuilder.Batchable<String> CREDENTIALS = csv("data/credentials.csv").circular();
+    private static final FeederBuilder.FileBased<Object> CREDENTIALS_FEEDER = jsonFile("data/users.json").circular();
 
     public static final ScenarioBuilder TRAINING_SCENARIO = scenario("Training scenario")
-            .feed(CREDENTIALS)
+            .feed(CREDENTIALS_FEEDER)
             .exec(LOGIN_REQUEST);
 
 }
